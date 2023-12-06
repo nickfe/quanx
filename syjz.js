bcb2251a9939d66453fda856d45e639036c97621
daniel <dcui0515@gmail.com>
2023-12-06 10:17:38 +0800

    feat(script): syjz test res

--- syjz.js
+++ syjz.js
@@ -13,12 +13,24 @@ const path = "/account/grant/vip/detail/";
 if (url.indexOf(path) > -1) {
 
   let obj = JSON.parse(body);
-  obj.data.is_buy = 1;
-  obj.data.isvip = 1;
-  obj.data.days = 30;
-  obj.data.vip = {
-    isvip: 1,
-    days: 60,
+  obj.data = {
+    ...obj.data,
+    is_buy: 1,
+    trial_vip: {
+      pre_date: '2023-12-06 00:00:00',
+      buy_date: 1,
+      id: 2069596,
+      last_buy_date: '2023-12-06 00:00:00',
+      finish_date_ios: '2099.12.06',
+      vip_type: 0,
+      date_type: 1,
+      buy_status: 1,
+      days: 9999,
+      auto_buy: 0,
+      pre_status: 1,
+      status: 1,
+      finish_date: '2099-12-06 00:00:00',
+    },
   };
   body = JSON.stringify(obj);
 }
