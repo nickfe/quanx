cefeaffee92a2da234b58599e3aeac41da2669de
daniel <dcui0515@gmail.com>
2023-12-15 15:58:01 +0800

    feat(script): xTerminal

---
+++ xTerminal.js
@@ -0,0 +1,14 @@
+/**
+ * XTerminal
+ * ^https://api.xterminal.cn/user url script-response-body https://raw.githubusercontent.com/nickfe/quanx/main/xTerminal.js
+ * hostname=api.xterminal.cn
+ */
+
+let body = $response.body
+let obj = JSON.parse(body)
+obj.data = {
+  ...obj.data,
+  memberEnd: "2099-12-31T23:59:59.999Z",
+}
+body = JSON.stringify(obj)
+$done({ body })
