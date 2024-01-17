d80d8b58d2e5a45a0520f5fe7767943c6f85f155
daniel <dcui0515@gmail.com>
2024-01-17 10:43:55 +0800

    feat(uPaste): update config

--- uPaste.js
+++ uPaste.js
@@ -11,8 +11,8 @@ let body = $response.body
 if (url.includes('native_config.json')) {
   body = body
     .replace(/("enable_rating":)\w+/g, '$1 false')
-    .replace(/("enable_free_promote":)\w+/g, '$1 true')
-    .replace(/("enable_promote":)\w+/g, '$1 true')
+    .replace(/("enable_free_promote":)\w+/g, '$1 false')
+    .replace(/("enable_promote":)\w+/g, '$1 false')
     .replace(/("promote_trigger_count":)\d+/g, '$1 999999')
 
   // let obj = JSON.parse(body)
