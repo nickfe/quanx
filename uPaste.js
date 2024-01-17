763ca2d35d9f46b72fa5fd51372c0435d6079e0b
daniel <dcui0515@gmail.com>
2024-01-17 10:38:14 +0800

    feat(uPaste): use regexp

--- uPaste.js
+++ uPaste.js
@@ -9,17 +9,23 @@ hostname=api.7littlemen.com
 const url = $request.url
 let body = $response.body
 if (url.includes('native_config.json')) {
+  body = body
+    .replace(/("enable_rating":)\w+/g, '$1 false')
+    .replace(/("enable_free_promote":)\w+/g, '$1 true')
+    .replace(/("enable_promote":)\w+/g, '$1 true')
+    .replace(/("promote_trigger_count":)\d+/g, '$1 999999')
+
   // let obj = JSON.parse(body)
-  let obj = body // 原返回值不是标准的JSON，带了尾逗号
-  obj = {
-    ...obj,
-    "rating_trigger_count":20,
-    "enable_rating":false,
-    "promote_trigger_count":999999,
-    "enable_free_promote":true,
-    "enable_promote":true,
-  }
-  body = JSON.stringify(obj)
+  // let obj = body // 原返回值不是标准的JSON，带了尾逗号
+  // obj = {
+  //   ...obj,
+  //   "rating_trigger_count":20,
+  //   "enable_rating":false,
+  //   "promote_trigger_count":999999,
+  //   "enable_free_promote":true,
+  //   "enable_promote":true,
+  // }
+  // body = JSON.stringify(obj)
 }
 $done({ body })
 
