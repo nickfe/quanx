41b8e9302a4ad33776381982c359ef362db49901
daniel <dcui0515@gmail.com>
2024-07-11 11:12:05 +0800

    feat(circle): adapt to new version

--- circle.js
+++ circle.js
@@ -5,7 +5,7 @@ circle
 **************************************
 
 [rewrite_local]
-^https?://circlereader.com/api/user/current url script-response-body https://raw.githubusercontent.com/nickfe/quanx/main/circle.js
+^https?://circlereader.com/api/user/(current|get) url script-response-body https://raw.githubusercontent.com/nickfe/quanx/main/circle.js
 
 [mitm]
 hostname = circlereader.com
@@ -14,13 +14,10 @@ hostname = circlereader.com
 
 
 const body = JSON.stringify({
-  "uid": "1",
-  "avatar": "",
+  ...JSON.parse($response.body),
   "expire": "2099-12-31T00:00:00",
-  "mail": "test@test.com",
   "roles": [
     "member"
   ],
-  "name": "永久会员"
 })
 $done({ body })
