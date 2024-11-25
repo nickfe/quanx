cd00b1e63b1815ae20a1efb163921e8263b91a08
daniel <dcui0515@gmail.com>
2024-11-25 17:33:45 +0800

    test: freeBookIds

--- wxread.js
+++ wxread.js
@@ -9,22 +9,7 @@ var body = JSON.parse($response.body);
 
 body = {
   ...body,
-  "startTime": 1732518242,
-  "expiredTime": 2524579200,
-  "expired": 0,
-  "isPaying": 1,
-  "permanent": 1,
-  "remainTime": 792060958,
-  "payingRemainTime": 792060958,
-  "mcardHint": "付费会员有效期至2050年",
-  "hintsForRecharge": {
-    "buttonTitle": "已是付费会员",
-    "buttonSubtitle": "会员有效期至2050年",
-    "predictedSavedMoney": 0,
-    "predictedChapterPrice": 0,
-    "pricePerMonth": 0,
-    "sendCoupons": 0
-  }
+  freeBookIds: ['3300106023', 3300106023]
 };
 
 $done({
