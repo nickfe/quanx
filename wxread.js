b2d36080cb71c85151f6e7681f749223e26b7679
daniel <dcui0515@gmail.com>
2024-11-25 15:20:05 +0800

    feat: wxread

---
+++ wxread.js
@@ -0,0 +1,32 @@
+/**
+  微信读书
+  # wxread
+  ^https:\/\/i\.weread\.qq\.com\/pay\/memberCardSummary url script-response-body https://raw.githubusercontent.com/nickfe/quanx/main/wxread.js
+  hostname=i.weread.qq.com
+ */
+
+var body = $response.body;
+
+body = {
+  ...body,
+  "startTime": 1732518242,
+  "expiredTime": 2524579200,
+  "expired": 0,
+  "isPaying": 1,
+  "permanent": 1,
+  "remainTime": 792060958,
+  "payingRemainTime": 792060958,
+  "mcardHint": "付费会员有效期至2050年",
+  "hintsForRecharge": {
+    "buttonTitle": "已是付费会员",
+    "buttonSubtitle": "会员有效期至2050年",
+    "predictedSavedMoney": 0,
+    "predictedChapterPrice": 0,
+    "pricePerMonth": 0,
+    "sendCoupons": 0
+  }
+};
+
+$done({
+  body: JSON.stringify(body)
+});
