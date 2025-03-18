8cc2036b5d85bc857ed01237adc5af1d779b8c55
daniel <dcui0515@gmail.com>
2025-03-18 15:27:09 +0800

    feat: dida api

---
+++ didaapi.js
@@ -0,0 +1,19 @@
+/******************************
+
+[rewrite_local]
+
+api\.dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/nickfe/quanx/main/didaapi.js
+
+[mitm] 
+
+hostname = dida365.com
+
+*******************************/
+
+var responseBody = $response.body;
+var jsonObject = JSON.parse(responseBody);
+jsonObject.proEndDate = "2034-08-28T13:13:16.000+0000";
+jsonObject.needSubscribe = false;
+jsonObject.pro = true;
+responseBody = JSON.stringify(jsonObject);
+$done(responseBody);
